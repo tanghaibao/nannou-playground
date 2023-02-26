94aa64fbe8abbaad93002b3f1e856c501ee808e3
Haibao Tang <tanghaibao@gmail.com>
2023-02-26 08:35:31 -0800

    Only plot up to certain number of points in history

--- nannou-brownian/src/main.rs
+++ nannou-brownian/src/main.rs
@@ -2,22 +2,28 @@ use nannou::prelude::*;
 use nannou::rand;
 use rand_distr;
 use rand_distr::Distribution;
+use std::collections::VecDeque;
+
+const POINTS: usize = 500;
 
 struct Model {
     rng: rand::rngs::ThreadRng,
     normal: rand_distr::StandardNormal,
-    points: Vec<Point2>,
+    points: VecDeque<Point2>,
 }
 
 impl Model {
     fn update(&mut self) {
-        let last = self.points.last().unwrap();
+        let last = self.points.iter().last().unwrap();
         let new = *last
             + vec2(
                 self.normal.sample(&mut self.rng),
                 self.normal.sample(&mut self.rng),
             ) * 4.0; //vec2(random_range(-1.0, 1.0), random_range(-1.0, 1.0));
-        self.points.push(new);
+        self.points.push_back(new);
+        if self.points.len() > POINTS {
+            self.points.pop_front();
+        }
     }
 
     fn draw(&self, draw: &Draw) {
@@ -33,7 +39,7 @@ fn main() {
 }
 
 fn model(_app: &App) -> Model {
-    let points = vec![pt2(0.0, 0.0)];
+    let points = VecDeque::from(vec![pt2(0.0, 0.0)]);
     let normal = rand_distr::StandardNormal;
     let rng = rand::thread_rng();
     Model {
