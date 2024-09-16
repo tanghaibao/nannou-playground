6102ce15a606987e3ead20c1ed0fb921aa0775e5
Haibao Tang <tanghaibao@gmail.com>
2024-09-15 22:58:11 -0700

    now working

--- src/archero/main.rs
+++ src/archero/main.rs
@@ -1,7 +1,5 @@
-use bevy::{
-    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
-    prelude::*,
-};
+use bevy::color::palettes::css::GOLD;
+use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 mod archer;
@@ -21,13 +19,14 @@ fn main() {
     App::new()
         .register_type::<ColorText>()
         .register_type::<FpsText>()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(FrameTimeDiagnosticsPlugin)
-        .add_plugin(WorldInspectorPlugin)
-        .add_plugin(ArcherPlugin)
-        .add_startup_system(setup)
-        .add_system(text_update_system)
-        .add_system(text_color_system)
+        .add_plugins((
+            DefaultPlugins,
+            FrameTimeDiagnosticsPlugin,
+            WorldInspectorPlugin::default(),
+            ArcherPlugin,
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, text_color_system)
         .run();
 }
 
@@ -45,18 +44,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 font_size: FONT_SIZE,
                 color: Color::WHITE,
             },
-        ) // Set the alignment of the Text
-        .with_text_alignment(TextAlignment::TOP_CENTER)
-        // Set the style of the TextBundle itself.
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            position: UiRect {
-                bottom: Val::Px(5.0),
-                right: Val::Px(15.0),
-                ..default()
-            },
-            ..default()
-        }),
+        ),
+        // Set the alignment of the Text
+        // .with_text_alignment(TextAlignment::TOP_CENTER)
+        // // Set the style of the TextBundle itself.
+        // .with_style(Style {
+        //     position_type: PositionType::Absolute,
+        //     position: UiRect {
+        //         bottom: Val::Px(5.0),
+        //         right: Val::Px(15.0),
+        //         ..default()
+        //     },
+        //     ..default()
+        // }),
         ColorText,
         Name::new("bevy"),
     ));
@@ -77,7 +77,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 TextStyle {
                     font: font,
                     font_size: FONT_SIZE,
-                    color: Color::GOLD,
+                    color: GOLD.into(),
                 },
             ),
         ]),
@@ -91,21 +91,14 @@ fn text_color_system(time: Res<Time>, mut query: Query<&mut Text, With<ColorText
     let seconds = time.elapsed_seconds();
 
     // Update the color of the first and only section.
-    text.sections[0].style.color = Color::Rgba {
-        red: (1.25 * seconds).sin() / 2.0 + 0.5,
-        green: (0.75 * seconds).sin() / 2.0 + 0.5,
-        blue: (0.50 * seconds).sin() / 2.0 + 0.5,
-        alpha: 1.0,
-    };
-}
-
-fn text_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
-    for mut text in &mut query {
-        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
-            if let Some(value) = fps.smoothed() {
-                // Update the value of the second section
-                text.sections[1].value = format!("{value:.2}");
-            }
-        }
-    }
+    let red = (1.25 * seconds).sin() / 2.0 + 0.5;
+    let green = (0.75 * seconds).sin() / 2.0 + 0.5;
+    let blue = (0.50 * seconds).sin() / 2.0 + 0.5;
+    let alpha = 1.0;
+    text.sections[0].style.color = Color::Srgba(Srgba {
+        red,
+        green,
+        blue,
+        alpha,
+    });
 }
